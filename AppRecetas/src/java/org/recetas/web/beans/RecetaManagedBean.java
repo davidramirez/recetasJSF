e47019d31ca27fb46a6aaf93dcf3df35d0296cc9
David Ramirez <davidramirez_28@hotmail.com>
2019-06-25 13:25:51 +0200

    html receta y base de su ManagedBean

---
+++ AppRecetas/src/java/org/recetas/web/beans/RecetaManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.recetas.web.beans;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author david
+ */
+@Named(value = "recetaManagedBean")
+@ViewScoped
+public class RecetaManagedBean {
+
+    /**
+     * Creates a new instance of RecetaManagedBean
+     */
+    public RecetaManagedBean() {
+    }
+    
+}
