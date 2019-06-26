ff1d8ee4b634b8d82a10cd71ceb682083fb67652
David Ramirez <davidramirez_28@hotmail.com>
2019-06-26 11:19:34 +0200

    Visualización de los utensilios de una receta

--- AppRecetas/src/java/org/recetas/web/beans/RecetaManagedBean.java
+++ AppRecetas/src/java/org/recetas/web/beans/RecetaManagedBean.java
@@ -6,11 +6,15 @@
 package org.recetas.web.beans;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import org.recetas.dominio.Ingrediente;
 import org.recetas.dominio.Receta;
+import org.recetas.service.RecetasServiceLocal;
 
 /**
  *
@@ -22,24 +26,67 @@ public class RecetaManagedBean implements Serializable {
 
     private Receta receta;
     
-    @Inject
-    private AppManagedBean app;
+    //Control de pantallas
+    private int pantallaActual = 0;
+    
+    public static final int PRINCIPAL_RECETA = 0;
+    public static final int LISTA_UTENSILIOS = 1;
+    public static final int LISTA_INGREDIENTES = 2;
+    public static final int PASOS_RECETAS = 3;
+    
+    
+    @EJB
+    private RecetasServiceLocal recetasService;
     /**
      * Creates a new instance of RecetaManagedBean
      */
     public RecetaManagedBean() {
-        
+        System.out.println("Constructor");
     }
     
     @PostConstruct
     public void cargaReceta(){
-        receta = app.getReceta();
-        
+        receta = recetasService.getRecetaById(1);
+        System.out.println("receta" + receta.getTitulo());
     }
 
     public Receta getReceta() {
         return receta;
     }
+
+    public int getPantallaActual() {
+        return pantallaActual;
+    }
+
     
     
+    public boolean isPantallaPrincipal(){
+        return this.pantallaActual == PRINCIPAL_RECETA;
+    }
+    
+    public boolean isPantallaListaUtensilios(){
+        return this.pantallaActual == LISTA_UTENSILIOS;
+    }
+    
+    public boolean isPantallaListaIngredientes(){
+        return this.pantallaActual == LISTA_INGREDIENTES;
+    }
+    
+    public boolean isPantallaPasos(){
+        return this.pantallaActual == PASOS_RECETAS;
+    }
+    
+    public void cambiarPantalla(int pantalla){
+        this.pantallaActual = pantalla;
+    }
+    
+    public List<String> getUtensiliosDeReceta(){
+        List<String> list = recetasService.getListaUtensilios(this.receta.getId());
+        //System.out.println("lista " + list +"***********");
+        return list;
+    }
+    
+    public List<Ingrediente> getIngredientesDeReceta(){
+        return recetasService.getListaIngredientes(this.receta.getId());
+    }
 }
